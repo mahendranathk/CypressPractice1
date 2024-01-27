feacf5188a5f143bd394a546456a509a716ad95b
Mahendranath K <mahendranathkundla13@gmail.com>
2024-01-27 11:14:57 +0530

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+import cypressSplit from 'cypress-split'
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      cypressSplit(on, config)
     },
   },
 });
